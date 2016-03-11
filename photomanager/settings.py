9b95afe67bb6837f6dc19baee5d08728afea44db
Carlos Cajero <ccajero@gmail.com>
2016-03-11 12:07:13 -0600

    Photo model

--- photomanager/settings.py
+++ photomanager/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photomanager'
+    'photos'
 ]
 
 MIDDLEWARE_CLASSES = [
--- photos/models.py
+++ photos/models.py
@@ -2,4 +2,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+COPYRIGHT = 'RIG'
+COPYLEFT = 'LEF'
+CREATIVE_COMMONS = 'CC'
+
+LICENCES = (
+    (COPYLEFT, 'Copyleft'),
+    (COPYRIGHT, 'Copyright'),
+    (CREATIVE_COMMONS, 'Commons')
+)
+
+class Photo(models.Model):
+
+    name = models.CharField(max_length=50)
+    url = models.URLField()
+    description = models.TextField(blank=True, null=True, default="")
+    created_at = models.DateTimeField(auto_now_add=True)  # Fecha automatica de agregado
+    modified_at = models.DateTimeField(auto_now=True)  # Fecha automatica de modificacion
+    license = models.CharField(max_length=3, choices=LICENCES)
